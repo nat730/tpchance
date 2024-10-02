22f627dadc4b4b2cfbadd5f32e77609ca728ac39
Louise Mendiburu <louisemendiburu@gmail.com>
2024-10-02 12:09:32 +0200

    functional betting game

--- back/src/index.ts
+++ back/src/index.ts
@@ -13,6 +13,8 @@ interface User {
   id: string;
   pseudo: string;
   jetons: number;
+  pileFace?: string;
+  mise?: number;
 }
 let userTab: User[] = [];
 
@@ -20,7 +22,6 @@ io.on('connection', (socket) => {
   console.log('a user connected', socket.id)
 
   socket.on('pseudo', pseudoValue => {
-    console.log(pseudoValue);
     const user = {
       id: socket.id,
       pseudo: pseudoValue,
@@ -29,20 +30,53 @@ io.on('connection', (socket) => {
     if (userTab.length < 3) {
       userTab.push(user);
       socket.emit("createUserSuccess");
+      console.log(userTab);
     } else {
       socket.emit("errorGameFull");
     }
-    console.log(userTab);
+    if (userTab.length === 3) {
+      io.emit("gameStart");
+
+      setTimeout(() => {
+        const jetons = userTab.find(user => user.id === socket.id)?.jetons;
+        io.emit("startManche", jetons);
+      }, 5000);
+    }
   })
 
   socket.on('sendMessage', message => {
     const sender = userTab.find(user => user.id === socket.id)?.pseudo;
-    
+
     const messageObj = {
       pseudo: sender,
       message: message
     };
     io.emit("chatMessage", messageObj);
+  })
+
+  socket.on('sendMise', mise => {
+    const userIndex = userTab.findIndex(user => user.id === socket.id);
+    userTab[userIndex].pileFace = mise.pileFaceValue;
+    userTab[userIndex].mise = parseInt(mise.miseValue);
+    console.log(userTab);
+
+    const playerHasBet = userTab.filter(user => user.pileFace !== undefined && user.mise !== undefined);
+    if (playerHasBet.length === 3) {
+      const pileFaceGame = Math.random();
+      let pileFaceResult = pileFaceGame < 0.5 ? "pile" : "face";
+
+      userTab.forEach(user => {
+        if (user.mise !== undefined) {
+          if (pileFaceResult === user.pileFace) {
+            user.jetons = user.jetons + user.mise + 10;
+            io.emit("win", user.id, user.jetons, user.mise);
+          } else {
+            user.jetons = user.jetons - user.mise;
+            io.emit("lose", user.id, user.jetons, user.mise);
+          }
+        }
+      })
+    }
   })
 
   socket.on('disconnect', () => {
--- front/script.js
+++ front/script.js
@@ -1,9 +1,16 @@
 const socket = io('http://localhost:3000')
+
+let timerId;
+let userId;
+
 socket.on('connect', () => {
     console.log('connected', socket.id)
+    userId = socket.id;
 })
 
 const body = document.querySelector("body");
+
+//Form pseudo
 const formPseudo = document.createElement("form");
 
 const inputPseudo = document.createElement("input");
@@ -14,6 +21,11 @@ const submitButtonPseudo = document.createElement("button");
 submitButtonPseudo.type = "submit";
 submitButtonPseudo.textContent = "Valider";
 
+formPseudo.appendChild(inputPseudo);
+formPseudo.appendChild(submitButtonPseudo);
+body.append(formPseudo);
+
+//Form chat
 const formChat = document.createElement("form");
 
 const inputChat = document.createElement("input");
@@ -24,11 +36,55 @@ const submitButtonChat = document.createElement("button");
 submitButtonChat.type = "submit";
 submitButtonChat.textContent = "Envoyer";
 
+//Chat container
 const chatContainer = document.createElement("div");
 
-formPseudo.appendChild(inputPseudo);
-formPseudo.appendChild(submitButtonPseudo);
-body.append(formPseudo);
+//Chrono
+const chronoValue = document.createElement("p");
+
+//Manche
+let pileFaceValue = "pile";
+const miseContainer = document.createElement("form");
+
+const pileButton = document.createElement("input");
+pileButton.type = "radio";
+pileButton.id = "pile";
+pileButton.name = "pari";
+pileButton.selected = "true";
+
+const pileLabel = document.createElement("label");
+pileLabel.setAttribute("for", "pile");
+pileLabel.innerHTML = "Pile";
+
+const faceButton = document.createElement("input");
+faceButton.type = "radio";
+faceButton.id = "face";
+faceButton.name = "pari";
+
+const faceLabel = document.createElement("label");
+faceLabel.setAttribute("for", "face");
+faceLabel.innerHTML = "Face";
+
+const miseInput = document.createElement("input");
+miseInput.type = "number";
+miseInput.placeholder = "Entrer votre mise";
+miseInput.min = 0;
+
+const submitButtonMise = document.createElement("button");
+submitButtonMise.type = "submit";
+submitButtonMise.textContent = "Valider";
+
+miseContainer.appendChild(pileButton);
+miseContainer.appendChild(pileLabel);
+miseContainer.appendChild(faceButton);
+miseContainer.appendChild(faceLabel);
+miseContainer.appendChild(miseInput);
+miseContainer.appendChild(submitButtonMise);
+
+const waiting = document.createElement("p");
+waiting.innerHTML = "En attente des autres joueurs...";
+
+const mancheEndMessage = document.createElement("p");
 
 submitButtonPseudo.addEventListener('click', function (e) {
     e.preventDefault();
@@ -58,12 +114,79 @@ submitButtonChat.addEventListener('click', function (e) {
 
 socket.on("chatMessage", messageObj => {
     const messageContainer = document.createElement("div");
+
     const sender = document.createElement("p");
     sender.innerHTML = messageObj.pseudo;
+
     const message = document.createElement("p");
     message.innerHTML = messageObj.message;
+
     messageContainer.appendChild(sender);
     messageContainer.appendChild(message);
 
     chatContainer.append(messageContainer);
 });
+
+socket.on("gameStart", () => {
+    chatContainer.remove();
+    formChat.remove(); 
+    mancheEndMessage.remove();
+
+    body.append(chronoValue);
+
+    const start = Date.now();
+
+    timerId = setInterval(() => {
+        const millis = Date.now() - start;
+        chronoValue.innerHTML = (millis / 1000).toFixed(3);
+    }, 137);
+});
+
+socket.on("startManche", jetons => {
+    clearInterval(timerId);
+    chronoValue.remove();
+
+    miseInput.max = jetons;
+    body.append(miseContainer);
+});
+
+pileButton.addEventListener('click', function (e) {
+    pileFaceValue = "pile";
+});
+
+faceButton.addEventListener('click', function (e) {
+    pileFaceValue = "face";
+});
+
+submitButtonMise.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const miseValue = miseInput.value;
+    const mise = {
+        pileFaceValue: pileFaceValue,
+        miseValue: miseValue
+    }
+    socket.emit("sendMise", mise);
+
+    miseContainer.remove();
+
+    body.append(waiting);
+});
+
+socket.on("win", (id, jetons, mise) => {
+    if (id === userId) {
+        waiting.remove();
+
+        mancheEndMessage.innerHTML = `Vous avez gagné ${mise + 10} jetons ! Vous avez désormais ${jetons} jetons.`;
+        body.append(mancheEndMessage);
+    }
+})
+
+socket.on("lose", (id, jetons, mise) => {
+    if (id === userId) {
+        waiting.remove();
+        
+        mancheEndMessage.innerHTML = `Vous avez perdu ${mise} jetons. Vous avez désormais ${jetons} jetons.`;
+        body.append(mancheEndMessage);
+    }
+})
